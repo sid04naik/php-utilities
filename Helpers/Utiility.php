ab69cbc35f86d1b185569c681e4d12d279ed7f65
sid04naik <siddhantnaik.dev@gmail.com>
2020-10-02 10:57:04 +0530

    added helper utlility class

---
+++ Helpers/Utiility.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * *Utility file which contains some most used and common php utility functions
+ * *All the utility functions at one place
+ * *Customizable
+ * @author Siddhant Naik (Software Engineer)
+ */
+namespace Helpers\Utility;
+
+/**
+ * *Utility class for all utility functions
+ */
+class Utility
+{
+}
